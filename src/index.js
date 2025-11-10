a6612785f5413e889d635310e8fba0852968b3c4
Buraque's Code <143602032+Buraquescode@users.noreply.github.com>
2025-11-10 11:13:02 +0500

    Db Connection and Errors Debugging

---
+++ src/index.js
@@ -0,0 +1,88 @@
+//require("dotenv").config();
+
+import dotev from "dotenv";
+import express from "express";
+const app = express();
+import connetDb from "./db/db.js";
+
+dotev.config({
+    path: "./.env"
+});
+
+connetDb();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+(async ()=> {
+    try {
+        await mongoose.connect(`process.env.MONGO_URL/${DB_Name}`)
+        
+        app.on("error",()=>{
+            console.log("Error", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT || 8000,()=>{
+            console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        })
+
+        console.log("Connected to the database successfully");
+
+    } catch (error) {
+        console.error("Error connecting to the database", error);
+        throw error;
+    }
+})()
+*/
+
